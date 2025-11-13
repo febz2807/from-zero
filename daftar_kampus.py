70e4379aea26b6c631df92806dc3e465f78cb59f
Febryanto Nugroho <febryantonugroho2807@gmail.com>
2025-11-13 15:22:27 +0700

    Add files via upload

---
+++ daftar_kampus.py
@@ -0,0 +1,10 @@
+# Project minggu pertama (ke: 1) 
+# Pendaftaran Mahasiswa Baru
+nama = input("Masukkan nama lengkap anda: ")
+umur = input("Masukkan umur anda: ")
+jurusan = input("Masukkan jurusan anda: ")
+universitas = input("Masukkan universitas anda: ")
+alamat  = input("Masukkan alamat lengkap anda: ")
+
+print("=========== HASIL =============")
+print(f"Nama saya {nama}, Usia saya {umur} tahun jurusan yang saya pilih {jurusan}, universitas yang saya mau {universitas} dan tempat tinggal saya berada di {alamat}")
