390d6ed1baf8d2e81c5e2eef1c454b1641710797
Febryanto Nugroho <febryantonugroho2807@gmail.com>
2025-11-13 15:13:47 +0700

    Add files via upload

---
+++ perkenalan.py
@@ -0,0 +1,9 @@
+nama = input("Masukkan nama anda: ")
+umur = input("Masukan umur anda: ")
+jurusan = input("Apa jurusan yang kamu pilih? ")
+universitas = input("Apa Universitas impian kamu? ")
+cita_cita = input("Apa cita cita kamu? ")
+
+print("============ Hasil =============")
+
+print(f" hallo nama saya {nama}, usia saya {umur} tahun, saya pilih jurusan {jurusan}, saya ingin kuliah di {universitas} dan cita cita saya menjadi seorang {cita_cita}")
