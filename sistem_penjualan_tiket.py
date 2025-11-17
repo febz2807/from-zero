658dc5565f7b503a6123657c7de6fc5580426043
Febryanto Nugroho <febryantonugroho2807@gmail.com>
2025-11-17 23:56:50 +0700

    Add files via upload

--- sistem_penjualan_tiket.py
+++ sistem_penjualan_tiket.py
@@ -11,27 +11,24 @@ class Transaksi:
         self.total = tiket.harga * jumlah
 
     def cetak_detail(self):
-        print("\n===== DETAIL TRANSAKSI =====")
+        print("\n=== Detail TRANSAKSI ====")
         print(f"Event        : {self.tiket.nama_event}")
-        print(f"Harga Tiket  : {self.tiket.harga}")
+        print(f"Harga tiket  : Rp {self.tiket.harga}")
         print(f"Jumlah       : {self.jumlah}")
-        print(f"Total Bayar  : {self.total}")
-        print('============================')
+        print(f"Total Bayar  : Rp {self.total}")
+        print("======================\n")
 
-# Membuat 3 objek tiket
 t1 = Tiket("Konser Musik", 150000)
 t2 = Tiket("Seminar AI", 100000)
-t3 = Tiket("Festival Kuliner", 50000)
+t3 = Tiket("Festival Kuliner", 500000)
 
-# Daftar event
-print("=== PILIH EVENT ===")
+print("== Pilih EVENT ===")
 print("1.", t1.nama_event, "-Rp", t1.harga)
 print("2.", t2.nama_event, "-Rp", t2.harga)
 print("3.", t3.nama_event, "-Rp", t3.harga)
 
-pilih = int(input("Pilih event 1-3: "))
+pilih = int(input("Pilih Event (1-3): "))
 
-# Menentukan tiket yang dipilih
 if pilih == 1:
     tiket_dipilih = t1
 elif pilih == 2:
@@ -40,12 +37,10 @@ elif pilih == 3:
     tiket_dipilih = t3
 else:
     print("Pilihan tidak valid!")
-    exit()
+    exit
 
 jumlah = int(input("Masukkan jumlah tiket: "))
 
-# Membuat transaksi
 transaksi = Transaksi(tiket_dipilih, jumlah)
 
-# Cetak detail transaksi
 transaksi.cetak_detail()
