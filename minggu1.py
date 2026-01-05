b6cb336feda8827b924d135d0b518e150622dd19
Febryanto Nugroho <febryantonugroho2807@gmail.com>
2026-01-06 00:03:32 +0700

    Add files via upload

---
+++ minggu1.py
@@ -0,0 +1,21 @@
+#nama = input("Masukkan Nama Anda: ")
+#umur = input("Masukkan Umur Anda: ")
+#alamat = input("Masukkan Alamat Anda: ")
+
+#print("===============")
+
+#print(f"halo nama saya: {nama}")
+#print(f"usia saya: {umur}")
+#print(f"alamat saya: {alamat}")
+
+nama = print(input("Masukkan Nama Anda: "))
+usia = print(input("Masukkan Umur Anda: "))
+
+umur = 16
+punya_ktp = True
+
+if umur >= 17 and punya_ktp:
+    print("Usia Anda Telah Mencukupi")
+else:
+    print("Usia Anda Belum Mencukupi")
+
